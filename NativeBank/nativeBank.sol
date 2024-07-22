1f2bc602e394d068a323f6e2e24b818e44b7af03
Cat <zedxlab@gmail.com>
2024-07-22 23:25:40 +0200

    Add files via upload

---
+++ NativeBank/nativeBank.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+import "./IBank.sol";
+
+contract NativeBank is IBank {
+
+    address public owner;
+    mapping(address account => uint balance) private _balances;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    receive() external payable { }
+
+    fallback() external payable {
+        this.deposit();
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Not owner");
+        _;
+    }
+
+    function balanceOf(address _account) external view returns(uint256) {
+        return _balances[_account];
+    }
+
+    function deposit() external payable {
+        _balances[msg.sender] += msg.value;
+
+        emit Deposit(msg.sender, msg.value);
+    }
+    
+    function withdraw(uint256 _amount) external {
+        if (_amount == 0) {
+            revert WithdrawalAmountZero(msg.sender);
+        }
+        if (_amount > _balances[msg.sender]) {
+            revert WithdrawalAmountExceedsBalance({
+            account: msg.sender, 
+            amount: _amount, 
+            balance: _balances[msg.sender]
+            });
+        }
+        
+        unchecked {
+            _balances[msg.sender] -= _amount;
+        }
+
+        (bool success,) = msg.sender.call{value: _amount}("");
+        require(success, "Failed to send Ether");
+
+        emit Withdrawal(msg.sender, _amount);
+    }
+
+    function withdrawAll() external onlyOwner{
+        uint256 contractBalane = address(this).balance;
+
+        (bool success,) = owner.call{value: contractBalane}("");
+        require(success, "Failed to send Ether");
+    }
+
+}
