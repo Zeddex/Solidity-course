1c9418d3236009d1f4357d901b5526789ac855b4
Zeddex <zedxlab@gmail.com>
2024-09-10 21:27:02 +0200

    fixes

--- 3. Tests/src/CustomToken.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.26;
-
-import "./ERC20.sol";
-
-contract CustomToken is ERC20 {
-    constructor(address owner) ERC20("CustomToken", "CSTM", 1000000, owner) {}
-}
